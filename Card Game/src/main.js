fb0b405dde18d7d51b4a18c0018489e56943e8ec
Michael Barrett <74513436+Barrineer@users.noreply.github.com>
2024-03-12 19:35:04 -0400

    Initial Commit
    Initial commit for card game project

---
+++ Card Game/src/main.js
@@ -0,0 +1,14 @@
+//Chess pieces are indicated with ascii 9812 - 9823
+//Card suites are indicated with ascii 9824 - 9831
+//Straight up PLAYING CARDS ascii 127136 - 127221
+/*
+
+
+
+
+*/
+var card = 
+""
+;
+
+console.log(card)
